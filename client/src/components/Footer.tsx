49b90d359365db90e8307f9e7923d68044f3a4a2
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-08-11 19:07:12 -0500

    Added react icons as a dependency in client. Updated footer with icons.

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
+import { AiFillLinkedin, AiFillGithub, AiFillInstagram } from 'react-icons/ai'
 
 const Footer = () => {
+    const elements: { name: string, url: string, icon: ReactNode }[] = [
+        { name: "LinkedIn", url: "https://www.linkedin.com", icon: <AiFillLinkedin /> },
+        { name: "GitHub", url: "https://www.github.com", icon: <AiFillGithub /> },
+        { name: "Instagram", url: "https://www.instagram.com", icon: <AiFillInstagram /> },
+    ];
+
     return (
         <footer id='footer'>
-            <p>This is my footer</p>
+            {elements.map((element) => {
+                return (
+                    <div key={element.name}>
+                        <a href={element.url}>
+                            {element.icon}
+                        </a>
+                    </div>
+                )
+            })}
         </footer>
     )
 }
