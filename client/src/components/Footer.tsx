572d8594ba65e407f521aa1d60874d3d506530f2
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-08-11 19:29:08 -0500

    Added more icons to footer, added more stylings with hover

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,11 +1,14 @@
 import React, { ReactNode } from 'react'
-import { AiFillLinkedin, AiFillGithub, AiFillInstagram } from 'react-icons/ai'
+import { AiFillLinkedin, AiFillGithub, AiFillInstagram, AiOutlineMail, AiOutlinePhone } from 'react-icons/ai'
+import { IconContext } from "react-icons";
 
 const Footer = () => {
     const elements: { name: string, url: string, icon: ReactNode }[] = [
+        { name: "Phone", url: "tel:+1-917-917-917", icon: <AiOutlinePhone /> },
         { name: "LinkedIn", url: "https://www.linkedin.com", icon: <AiFillLinkedin /> },
         { name: "GitHub", url: "https://www.github.com", icon: <AiFillGithub /> },
         { name: "Instagram", url: "https://www.instagram.com", icon: <AiFillInstagram /> },
+        { name: "Email", url: "mailto:yourcompany@gmail.com", icon: <AiOutlineMail /> }
     ];
 
     return (
@@ -13,8 +16,10 @@ const Footer = () => {
             {elements.map((element) => {
                 return (
                     <div key={element.name}>
-                        <a href={element.url}>
-                            {element.icon}
+                        <a className='link' href={element.url}>
+                            <IconContext.Provider value={{ className: "footer-icon" }}>
+                                {element.icon}
+                            </IconContext.Provider>
                         </a>
                     </div>
                 )
