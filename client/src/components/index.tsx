926bb4d29194694bf0f1e3c51d17d4d61106d5cf
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-08-12 19:45:36 -0500

    Added react-hamburger-menu dependency and hamburger menu

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -1,6 +1,7 @@
 import Navigation from "./Navigation";
 import About from "./About";
 import Footer from "./Footer";
+import Hamburger from "./Hamburger";
 
 
-export { Navigation, About, Footer };
+export { Navigation, About, Footer, Hamburger };
