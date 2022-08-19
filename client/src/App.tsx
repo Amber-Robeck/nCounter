4d7d5ca116c70f42a51128feb84d6cbda3035cae
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-08-18 19:38:11 -0500

    Added basic homepage component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './assets/css/App.css';
-import { Navigation, About, Footer, Hamburger } from './components/index';
+import { Navigation, About, Footer, Hamburger, Home } from './components/index';
 
 function App() {
   return (
     <div id='app' className="App">
       <Navigation />
       <Hamburger />
+      <Home />
       <About />
       <Footer />
     </div>
--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -2,6 +2,7 @@ import Navigation from "./Navigation";
 import About from "./About";
 import Footer from "./Footer";
 import Hamburger from "./Hamburger";
+import Home from "./Home";
 
 
-export { Navigation, About, Footer, Hamburger };
+export { Navigation, About, Footer, Hamburger, Home };
